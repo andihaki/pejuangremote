3b8c807fdfd541f4e1ab634e86a134a8c44a7aa1
Andi Hakim Arif <andi.hakim.arif@gmail.com>
2020-10-10 15:55:37 +0700

    bismillah, init project

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+const Index = () => {
+    return (
+            <div style={{ margin: '0 auto', maxWidth: '500px', width: '100%', textAlign: 'center' }}>
+                <h1>👋 Hallo, Pengguna PejuangRemote</h1>
+                <p>Website ini masih dalam tahap pengembangan, harap bersabar ya 🙏 </p>
+            </div>
+    );
+}
+
+export default Index;
